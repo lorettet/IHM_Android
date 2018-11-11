bdb2b4fd08acd972e02d8859a329205d6ee1c7c7
Théo <theo.lorette-froidevaux@insa-lyon.fr>
2018-11-11 15:38:22 +0100

    Add map to map fragment

--- app/src/main/java/fr/insa_lyon/ihm_android/MainActivity.java
+++ app/src/main/java/fr/insa_lyon/ihm_android/MainActivity.java
@@ -1,15 +1,25 @@
 package fr.insa_lyon.ihm_android;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    private static final int ERROR8DIALOG8REQUEST = 9001;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,7 +30,9 @@ public class MainActivity extends AppCompatActivity {
             Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_map:
-                    selectedFragment = new MapFragment();
+                    if(isServicesOK()) {
+                        selectedFragment = new MapFragment();
+                    }
                     break;
                 case R.id.navigation_directions:
                     selectedFragment = new DirectionsFragment();
@@ -41,6 +53,28 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: cheking google services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+        
+        if(available == ConnectionResult.SUCCESS)
+        {
+            // everything fine!
+            Log.d(TAG, "isServicesOK: Google Play Services is working");
+            return true;
+        }else if (GoogleApiAvailability.getInstance().isUserResolvableError(available))
+        {
+            // Issue fixable
+            Log.d(TAG, "isServicesOK: an error occured but is fixable");
+            Dialog dialog =  GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this,available,ERROR8DIALOG8REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(MainActivity.this,"This will not work :(",Toast.LENGTH_SHORT);
+        }
+        return false;
     }
 
 }
