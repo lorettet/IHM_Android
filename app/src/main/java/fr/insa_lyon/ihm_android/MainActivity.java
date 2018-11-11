c03e8185f0a23314c169ddb8909b2b6e66c6c846
Théo <theo.lorette-froidevaux@insa-lyon.fr>
2018-11-11 14:46:06 +0100

    First Commit

---
+++ app/src/main/java/fr/insa_lyon/ihm_android/MainActivity.java
@@ -0,0 +1,46 @@
+package fr.insa_lyon.ihm_android;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_map:
+                    selectedFragment = new MapFragment();
+                    break;
+                case R.id.navigation_directions:
+                    selectedFragment = new DirectionsFragment();
+                    break;
+                case R.id.navigation_group:
+                    selectedFragment = new GroupFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
